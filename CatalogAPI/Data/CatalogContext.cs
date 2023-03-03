c0e6844747d971ee42391b32ceeb5e4c2ee9da53
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-03 00:30:45 -0300

    Add context seed

--- CatalogAPI/Data/CatalogContext.cs
+++ CatalogAPI/Data/CatalogContext.cs
@@ -5,10 +5,12 @@ namespace CatalogAPI.Data
 {
     public class CatalogContext : ICatalogContext
     {
-        public CatalogContext(IConfiguration configuration) {
+        public CatalogContext(IConfiguration configuration) 
+        {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSedd.SeedData(Products);
         } 
 
         public IMongoCollection<Product> Products { get; } 
---
+++ CatalogAPI/Data/CatalogContextSedd.cs
@@ -0,0 +1,42 @@
+﻿using CatalogAPI.Entities;
+using MongoDB.Driver;
+
+namespace CatalogAPI.Data
+{
+    public class CatalogContextSedd
+    {
+        public static void SeedData(IMongoCollection<Product> productCollettion)
+        {
+            bool existProduct = productCollettion.Find(p => true).Any();
+            if (!existProduct) 
+            {
+                productCollettion.InsertManyAsync(GetMyProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetMyProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Id = "f01d9df2-b972-11ed-afa1-0242ac120002",
+                    Name = "Iphone 14",
+                    Description = "Telephone",
+                    Image = "product-1.png",
+                    Price = 7000.00M,
+                    Category = "Smart Phone"
+                },
+                new Product()
+                {
+                    Id = "7ce5bfa8-b973-11ed-afa1-0242ac120002",
+                    Name = "PS5",
+                    Description = "Console",
+                    Image = "product-2.png",
+                    Price = 4000.00M,
+                    Category = "Console"
+                }
+            };
+        }
+    }
+}
