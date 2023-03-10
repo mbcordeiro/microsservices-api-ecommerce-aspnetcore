39573471c4c40bc7a6123d8bf5eef9b0ccad933f
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-10 19:23:55 -0300

    Config redis

--- BasketAPI/Program.cs
+++ BasketAPI/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("ConnectionString");
+});
 
 var app = builder.Build();
 
