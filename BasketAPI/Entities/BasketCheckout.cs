5ce758878ede5b78e79d053fd100ecbf0cfb3b0a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-10 19:17:08 -0300

    Add entities

---
+++ BasketAPI/Entities/BasketCheckout.cs
@@ -0,0 +1,19 @@
+﻿namespace BasketAPI.Entities
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set; }
+        public string EmailAddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
