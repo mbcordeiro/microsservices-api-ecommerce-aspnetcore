252fa7e3c9e18e6f80f3c51311e4a8955f13391a
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2023-03-12 15:22:09 -0300

    Configure discount grpc

--- DiscountGrpc/Program.cs
+++ DiscountGrpc/Program.cs
@@ -1,4 +1,4 @@
-using DiscountGrpc.Services;
+using DiscountGrpc.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ DiscountGrpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using DiscountGrpc.Entities;
+
+namespace DiscountGrpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
